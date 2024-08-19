f7f77f2364dfbe140d950aa5883afe97d4ef4731
biboyd <brendanboyd16@gmail.com>
2024-08-19 13:54:13 -0700

    save more fields in all profile

--- save_all_profile.py
+++ save_all_profile.py
@@ -6,21 +6,15 @@ from sys import argv
 def _mass(field, data):
     return data["boxlib", "rho"]*unyt.g/unyt.cm**3 *data['boxlib', 'volume']
 def save_profile(fname):
-    ds = yt.load(fname, hint='amrex')
-    ds.add_field(name=("gas", "mass"),
-                function=_mass,
-                take_log=False,
-                display_name="$m$ ",
-                units='g',
-                sampling_type="local")
+    ds = yt.load(fname, hint='maestro')
     #generate profiles
     #prof_fields=[('boxlib', 'X(ne23)'), ('boxlib', 'X(na23)'), ('boxlib', 'X(c12)'), ('boxlib', 'X(o16)'), ('boxlib', 'tfromp'), ('boxlib', 'vort'), ('boxlib', 'radial_velocity'), ('boxlib', 'rho'), ('boxlib', 'p0pluspi'), ('boxlib', 'Hnuc'), ('boxlib', 'entropy')]
     
-    prof_fields = ds.field_list + [("gas", "mass")]
-    prof = yt.create_profile(ds.all_data(), 'radius', prof_fields, n_bins=500, extrema={'radius':(0, 8e7)}, logs={'radius':False})
+    prof_fields = ds.field_list + [("gas", "mass"), ("gas", "radial_velocity"), ("gas", "tangential_velocity"), ("gas", "velocity_magnitude")]
+    prof = yt.create_profile(ds.all_data(), 'radius', prof_fields, n_bins=600, extrema={'radius':(0, 8e7)}, logs={'radius':False})
 
     #save as csv for easy access later
-    df = prof.to_dataframe().to_csv(f"profiles/{ds.basename}_all_fields.csv")
+    df = prof.to_dataframe(include_std=True).to_csv(f"profiles/{ds.basename}_all_fields.csv")
     
 if __name__ == '__main__':
     #load and add mass field
