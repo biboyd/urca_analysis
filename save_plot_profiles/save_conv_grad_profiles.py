79632baf15ff206cbbf0ad2c19e1818f2bff0f0e
biboyd <boyd.brendan@stonybrook.edu>
2024-12-02 10:57:14 -0500

    fix errs in conv grad profiling

--- save_plot_profiles/save_conv_grad_profiles.py
+++ save_plot_profiles/save_conv_grad_profiles.py
@@ -1,14 +1,13 @@
 import yt
 import pandas as pd
-import matplotlib.pyplot
-import numpy as np
+import matplotlib.pyplot as plt
 from sys import argv
 
-@yt.derived_field(name=("gas", "ratio_ad"), sampling_type='local')
+@yt.derived_field(name=("boxlib", "ratio_ad"), sampling_type='local')
 def _ratio_ad(field, data):
   return data['boxlib', 'del']/data['boxlib', 'del_ad']
 
-@yt.derived_field(name=("gas", "ratio_ledoux"), sampling_type='local')
+@yt.derived_field(name=("boxlib", "ratio_ledoux"), sampling_type='local')
 def _ratio_led(field, data):
   return data['boxlib', 'del']/data['boxlib', 'del_ledoux']
 
@@ -16,28 +15,26 @@ def _ratio_led(field, data):
 def plot_conv_grad(df):
   fig, ax = plt.subplots(1, 1)
 
-  ax.plot(df0['radius']/1e5, df0['ratio_ledoux'],  
+  ax.plot(df['radius']/1e5, df['ratio_ledoux'],  
         color='tab:blue', label='Ledoux')
 
-  ax.plot(df0['radius']/1e5, df0['ratio_ad'], 
+  ax.plot(df['radius']/1e5, df['ratio_ad'], 
         color='tab:blue', alpha=0.7, ls='--', 
         lw=4, label='Adiabat')
   ax.set_xlim(0., 650.)
-  ax.set_ylim(-2.75, 1.2)
 
   ax.set_xlabel("Radius (km)")
   ax.set_ylabel("$\\nabla / \\nabla_{\\mathrm{conv}}$")
-  ax.legend(framealpha=1)
   ax.grid()
   ax.minorticks_on()
   #add surrounding ticks
   ax.tick_params(axis='both', direction='in', which='both', top=True, right=True)
 
   b,t = ax.get_ylim()
-  ax.vlines(conv_size_arr[-2, 1], b,t, color='black', lw=2, alpha=0.5, zorder=1)
-  ax.vlines(415, b, t, colors='black', zorder=-1, alpha=0.5, lw=2, linestyle='-.')
+  ax.vlines(415, b, t, colors='black', label='Urca Shell', zorder=-1, alpha=0.5, lw=2, linestyle='-.')
 
   ax.set_ylim(b, t)
+  ax.legend(framealpha=1)
 
   fig.tight_layout()
   fig.savefig("convective_gradient_ratio.png", dpi=300)
@@ -59,5 +56,5 @@ def main(ds):
 
 
 if __name__ == "__main__":
-  ds = yt.load(argv[0])
+  ds = yt.load(argv[1])
   main(ds)
